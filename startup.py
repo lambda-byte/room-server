11e4a82a9a0963dbba30c69fe1c431a0dcf0c108
lambda <elijah.soule@gmail.com>
2022-04-08 23:49:31 -0700

    terrible code

--- startup.py
+++ startup.py
@@ -2,17 +2,18 @@
 
 import os
 
-print("starting server")
+
 
 # Terrible code that will only work on linux systems. Should be moved to subprocess asap 
 
 username=os.system("whoami > /dev/null")
-if username is not "root":
+if username != "root":
     print ("You aren't root. This script cant use port 80 otherwise.")
     exit()
 
 # run if in root
 
+print("starting server")
 os.system("flask run --host 0.0.0.0 --port 80")
 
 # profit? 
