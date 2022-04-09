6f5d950ed6946a3ec6a550155d8c41b3fbbf39e0
lambda <elijah.soule@gmail.com>
2022-04-08 23:42:15 -0700

    back to basics again

--- startup.py
+++ startup.py
@@ -1,17 +1,11 @@
 # why not do this instead of a shell script
 
-# import things
-
-import subprocess
-
+import os
 
 print("starting server")
 
 # for this command you have to use sudo. not a good idea but this is not prod anyways.
 
-try:
-    subprocess.run(['flask run --host 0.0.0.0 --port 80'], check = True)
-except subprocess.CalledProcessError:
-    print ('Run failed. Install flask')
+os.system("flask run --host 0.0.0.0 --port 80")
 
 # profit? 
