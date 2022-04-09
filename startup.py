6d9eeaff1b7edf61c551bac4b9bc7cdec536f31a
lambda <elijah.soule@gmail.com>
2022-04-08 23:41:25 -0700

    back to the basics

--- startup.py
+++ startup.py
@@ -2,23 +2,16 @@
 
 # import things
 
-import os, subprocess
+import subprocess
 
 
 print("starting server")
 
-
-# this is stupid but it works
-class NotSudo(Exception):
-    pass
-
-if os.getuid() != 0:
-    raise NotSudo("You need superuser perms to run this. Port 80 wont be allocated otherwise.")
-
+# for this command you have to use sudo. not a good idea but this is not prod anyways.
 
 try:
     subprocess.run(['flask run --host 0.0.0.0 --port 80'], check = True)
 except subprocess.CalledProcessError:
-    print ('Run failed. Install flask.')
+    print ('Run failed. Install flask')
 
 # profit? 
