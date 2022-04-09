d6bc5beb67d5e576f448648a5545b5c3ca9ddb47
lambda <elijah.soule@gmail.com>
2022-04-09 13:14:02 -0700

    Aight lets get this rolling

--- .flaskenv
+++ .flaskenv
@@ -1,2 +1,3 @@
 FLASK_ENV=development
 FLASK_APP=room.py
+FLASK_RUN_PORT=80
--- startup.py
+++ startup.py
@@ -3,16 +3,6 @@
 import os
 
 
-
-# Terrible code that will only work on linux systems. Should be moved to subprocess asap 
-
-username=os.system("whoami > /dev/null")
-if username != "root":
-    print ("You aren't root. This script cant use port 80 otherwise.")
-    exit()
-
-# run if in root
-
 print("starting server")
 os.system("flask run --host 0.0.0.0 --port 80")
 
