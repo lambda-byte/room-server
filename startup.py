3574c398759d6b860002c174eb7b8d64845359e6
lambda <elijah.soule@gmail.com>
2022-04-08 23:44:42 -0700

    stupid dumb dumb hack

--- startup.py
+++ startup.py
@@ -6,6 +6,13 @@ print("starting server")
 
 # for this command you have to use sudo. not a good idea but this is not prod anyways.
 
+username=os.system("whoami")
+if username is not "root":
+    print ("You aren't root. This script cant use port 80 otherwise.")
+    exit()
+
+# run if in root
+
 os.system("flask run --host 0.0.0.0 --port 80")
 
 # profit? 
