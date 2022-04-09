ef557e362817e42eb590ce0def33d9ba2430b72e
lambda <elijah.soule@gmail.com>
2022-04-08 23:26:42 -0700

    Thing that might work

--- startup.py
+++ startup.py
@@ -1,9 +1,17 @@
 # why not do this instead of a shell script
-import os
+
+# import things
+
+import subprocess
+
 
 print("starting server")
 
 # for this command you have to use sudo. not a good idea but this is not prod anyways.
-os.system("flask run --host 0.0.0.0 --port 80")
+
+try:
+    subprocess.run(['flask run --host 0.0.0.0 --port 80'], check = True)
+except subprocess.CalledProcessError:
+    print ('Run failed. Try running with sudo')
 
 # profit? 
