22a9493a238fa35d15c523c3edeb5347a4c1fe0c
lambda <elijah.soule@gmail.com>
2022-04-09 13:36:34 -0700

    hope it works

--- install.py
+++ install.py
@@ -2,7 +2,10 @@
 # this is super wacky code
 # this will prob only work on a debian or debian based distro
 # please learn from my pain and suffering.
+# this code also completely opts out from using a virtualenv
+# you also need to run this with sudo lol
+
 
 import os, subprocess
 
-subprocess.run(["apt", "install", "postgresql postgresql-client", ""])
+subprocess.run(["apt", "install", "postgresql", "postgresql-client", "libpq-dev", "python3-dev", "-y"])
